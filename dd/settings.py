ec8eb0cd8a1af9143672ff649244f1f0c6835326
neonfoxik <neonfoxik1@gmail.com>
2026-02-10 20:04:11 +0300

    fixDB

--- dd/settings.py
+++ dd/settings.py
@@ -101,9 +101,9 @@ else:
             'HOST': os.getenv('DB_HOST', 'localhost'),
             'PORT': os.getenv('DB_PORT', '3306'),
             'OPTIONS': {
-                'init_command': "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci, sql_mode='STRICT_TRANS_TABLES'",
                 'charset': 'utf8mb4',
                 'use_unicode': True,
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             }
         }
     }
